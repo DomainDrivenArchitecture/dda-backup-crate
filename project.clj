2275452a00cdfc55554dc63e174f656158144624
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-01-31 21:03:16 +0100

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
-(defproject org.domaindrivenarchitecture/dda-backup-crate "0.2.1"
+(defproject org.domaindrivenarchitecture/dda-backup-crate "0.2.2-SNAPSHOT"
   :description "A crate to handle configuration, their dependencies, documentation and validation"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
  :dependencies [[org.clojure/clojure "1.6.0"]
-                [com.palletops/pallet "0.8.0-RC.11"]
-                [com.palletops/pallet "0.8.0-RC.11" :classifier "tests"]
+                [com.palletops/pallet "0.8.10"]
+                [com.palletops/pallet "0.8.10" :classifier "tests"]
                 [com.palletops/stevedore "0.8.0-beta.7"]
-                [org.domaindrivenarchitecture.org/dda-config-crate "0.2.0"]]
+                [org.domaindrivenarchitecture.org/dda-config-crate "0.3.0-SNAPSHOT"]]
  :repositories [["snapshots" :clojars]
                 ["releases" :clojars]]
  :deploy-repositories [["snapshots" :clojars]
                        ["releases" :clojars]]
  :profiles {:dev
             {:dependencies
-             [[com.palletops/pallet "0.8.0-RC.11" :classifier "tests"]
+             [[com.palletops/pallet "0.8.10" :classifier "tests"]
               ]
              :plugins
              [[com.palletops/pallet-lein "0.8.0-alpha.1"]]}
