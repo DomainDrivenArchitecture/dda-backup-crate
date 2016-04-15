59b5383d2ca13aec2a9c9ef71d29d01b9e497086
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-04-15 09:49:31 +0200

    lein-release plugin: preparing 0.3.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/dda-backup-crate "0.3.0-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/dda-backup-crate "0.3.0"
   :description "A crate to handle configuration, their dependencies, documentation and validation"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
@@ -7,9 +7,8 @@
                 [prismatic/schema "1.0.5"]
                 [metosin/schema-tools "0.7.0"]
                 [com.palletops/pallet "0.8.11"]
-                [com.palletops/pallet "0.8.10" :classifier "tests"]
                 [com.palletops/stevedore "0.8.0-beta.7"]
-                [org.domaindrivenarchitecture/dda-config-crate "0.3.1-SNAPSHOT"]]
+                [org.domaindrivenarchitecture/dda-config-crate "0.3.1"]]
  :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
  :deploy-repositories [["snapshots" :clojars]
@@ -17,7 +16,7 @@
  :profiles {:dev
             {:dependencies
              [[com.palletops/pallet "0.8.11" :classifier "tests"]
-              [org.domaindrivenarchitecture/dda-pallet-commons "0.1.0-SNAPSHOT" :classifier "tests"]]
+              [org.domaindrivenarchitecture/dda-pallet-commons "0.1.0" :classifier "tests"]]
              :plugins
              [[com.palletops/pallet-lein "0.8.0-alpha.1"]]}
              :leiningen/reply
