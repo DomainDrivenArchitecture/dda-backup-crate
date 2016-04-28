d279be50a96b9bf2e437a0b08c226f47504e293b
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-04-28 11:03:19 +0200

    lein-release plugin: preparing 0.3.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/dda-backup-crate "0.3.2-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/dda-backup-crate "0.3.2"
   :description "A crate to handle configuration, their dependencies, documentation and validation"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
@@ -6,18 +6,18 @@
  :dependencies [[org.clojure/clojure "1.7.0"]
                 [prismatic/schema "1.1.0"]
                 [metosin/schema-tools "0.9.0"]
-                [com.palletops/pallet "0.8.11"]
+                [com.palletops/pallet "0.8.12"]
                 [com.palletops/stevedore "0.8.0-beta.7"]
-                [org.domaindrivenarchitecture/dda-config-commons "0.1.1-SNAPSHOT"]
-                [org.domaindrivenarchitecture/dda-config-crate "0.3.2-SNAPSHOT"]]
+                [org.domaindrivenarchitecture/dda-config-commons "0.1.1"]
+                [org.domaindrivenarchitecture/dda-config-crate "0.3.2"]]
  :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
  :deploy-repositories [["snapshots" :clojars]
                        ["releases" :clojars]]
  :profiles {:dev
             {:dependencies
-             [[com.palletops/pallet "0.8.11" :classifier "tests"]
-              [org.domaindrivenarchitecture/dda-pallet-commons "0.1.1" :classifier "tests"]]
+             [[com.palletops/pallet "0.8.12" :classifier "tests"]
+              [org.domaindrivenarchitecture/dda-pallet-commons "0.1.2" :classifier "tests"]]
              :plugins
              [[com.palletops/pallet-lein "0.8.0-alpha.1"]]}
              :leiningen/reply
