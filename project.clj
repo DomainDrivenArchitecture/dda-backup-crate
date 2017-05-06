767922aafbabf45ff39b1cfe545568dbdd7610a1
luccr1 <lucca.hellriegel@gmx.net>
2017-05-06 14:40:54 +0200

    changed schema version back to 1.1.3

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [prismatic/schema "1.1.5"]
+                 [prismatic/schema "1.1.3"]
                  [com.palletops/pallet "0.8.12"]
                  [com.palletops/stevedore "0.8.0-beta.7"]
                  [org.domaindrivenarchitecture/dda-config-commons "0.1.7"]
