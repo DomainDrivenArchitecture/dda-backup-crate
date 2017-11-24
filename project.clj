b7334c78ff71d1b174dd5367f3c1e6ba168b4613
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-11-24 16:24:12 +0100

    updated deps

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [dda/dda-pallet "0.6.1"]
+                 [dda/dda-pallet "0.6.2"]
                  [dda/dda-user-crate "0.7.0"]
-                 [selmer "1.11.2"]]
+                 [selmer "1.11.3"]]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
   :deploy-repositories [["snapshots" :clojars]
@@ -16,7 +16,7 @@
               :dependencies
               [[org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
                [com.palletops/pallet "0.8.12" :classifier "tests"]
-               [dda/dda-pallet-commons "0.5.1" :classifier "tests"]
+               [dda/dda-pallet-commons "0.6.0-SNAPSHOT" :classifier "tests"]
                [ch.qos.logback/logback-classic "1.2.3"]
                [org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
               :plugins
