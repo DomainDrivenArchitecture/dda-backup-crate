9ba435b47ae675fdcd792141e142bd71795bf68b
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-12-07 18:01:25 +0100

    lein-release plugin: preparing 0.7.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-backup-crate "0.7.0-SNAPSHOT"
+(defproject dda/dda-backup-crate "0.7.0"
   :description "A crate to handle backups"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
