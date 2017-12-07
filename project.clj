c25df7ce67003eca38357aae84a5ff547ccdaa4f
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-12-07 18:01:50 +0100

    lein-release plugin: bumped version from 0.7.0 to 0.7.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-backup-crate "0.7.0"
+(defproject dda/dda-backup-crate "0.7.1-SNAPSHOT"
   :description "A crate to handle backups"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
