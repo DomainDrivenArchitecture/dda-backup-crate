ee4a384a088cabedc5e675460a9b33393ba81cd6
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-12-13 09:18:23 +0100

    Version 0.7.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-backup-crate "0.7.1-SNAPSHOT"
+(defproject dda/dda-backup-crate "0.7.1"
   :description "A crate to handle backups"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
