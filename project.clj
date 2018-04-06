b637138eb1522bbe492e6d075acf51e799784874
jem <michael.jerger@meissa-gmbh.de>
2018-04-06 11:51:12 +0200

    updated deps & fixed project.clj

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject dda/dda-backup-crate "0.7.2-SNAPSHOT"
+(defproject dda/dda-backup-crate "1.0.0-SNAPSHOT"
   :description "A crate to handle backups"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [dda/dda-pallet "0.6.5"]
-                 [dda/dda-user-crate "0.7.0"]
-                 [selmer "1.11.3"]]
+                 [dda/dda-pallet "2.1.1"]
+                 [dda/dda-user-crate "1.0.3-SNAPSHOT"]
+                 [selmer "1.11.7"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
@@ -19,10 +19,11 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
+                   [[org.clojure/test.check "0.10.0-alpha2"]
+                    [org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
                     [com.palletops/pallet "0.8.12" :classifier "tests"]
-                    [ch.qos.logback/logback-classic "1.2.3"]
-                    [org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
+                    [ch.qos.logback/logback-classic "1.3.0-alpha4"]
+                    [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]
                    :plugins [[lein-sub "0.3.0"]
                              [lein-pprint "1.1.2"]]
                    :leiningen/reply  {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-alpha2"
@@ -33,6 +34,8 @@
              :uberjar {:source-paths ["uberjar/src"]
                        :resource-paths ["uberjar/resources"]
                        :aot :all
-                       :main dda.pallet.dda-serverspec-crate.main
-                       :dependencies [[org.clojure/tools.cli "0.3.5"]]}}
+                       :main dda.pallet.dda-backup-crate.main
+                       :dependencies [[org.clojure/tools.cli "0.3.5"]
+                                      [ch.qos.logback/logback-classic "1.3.0-alpha4"]
+                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]}}
   :local-repo-classpath true)
