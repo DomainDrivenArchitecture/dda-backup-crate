7d27e06476a8017e6cbba9e4322b38c086128485
jem <michael.jerger@meissa-gmbh.de>
2018-04-13 10:37:02 +0200

    started release by accident

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-backup-crate "1.0.0"
+(defproject dda/dda-backup-crate "1.0.1-SNAPSHOT"
   :description "A crate to handle backups"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
